8e7e299ec8bd410213857afb55de6d4f50554b9d
Aman kumar <87026675+Aman300@users.noreply.github.com>
2022-05-24 13:41:47 +0530

    new add

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 //// IMPORTING CONTROLLERS  /////
-// const userController= require("../controllers/userController");
+const { createUser }= require("../controller/userController");
 // const bookController = require("../controllers/bookController")
 // const reviewController = require("../controllers/reviewController")
 // const middleware = require ("../middleware/auth")
 
 //////
+
+router.get('/', createUser)
 
 module.exports = router;
