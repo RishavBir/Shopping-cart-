4e9147893778771b27181f83ee826bb738be6b77
Aman kumar <87026675+Aman300@users.noreply.github.com>
2022-06-01 12:51:17 +0530

    cart controller done

--- src/models/cartModel.js
+++ src/models/cartModel.js
@@ -1,15 +1,43 @@
-const mongoose = require('mongoose');
-const ObjectId = mongoose.Schema.Types.ObjectId;
+const mongoose = require("mongoose")
+const ObjectId = mongoose.Schema.Types.ObjectId
 
-const cartSchema = new mongoose.Schema({ 
-  userId: {type: ObjectId,ref: 'User',required: true,trim: true,},
-  items: [{
-    productId: {type: ObjectId,ref: 'Products',required: true,trim: true,},
-    quantity: {type: Number,required: true,trim: true,min: 1},
-    _id: false
-  }],
-  totalPrice: {type: Number,required: true,trim: true},
-  totalItems: {type: Number,required: true,trim: true},
-},{timestamps: true});
+const cartSchema = new mongoose.Schema({
 
-module.exports = mongoose.model('Cart', cartSchema); //carts
+    userId: {
+        type:ObjectId, 
+        ref:'User',
+        required:true , 
+        unique:true
+    },
+
+    items: [{
+        productId:{
+        required:true ,
+        type:ObjectId,
+        ref:'Products'
+    },
+
+        quantity: {
+            type:Number, 
+            required:true , 
+            default :1
+            },
+    }],
+
+    totalPrice: {
+        type:Number, 
+        required:true ,
+        comment: "Holds total price of all the items in the cart"
+    },
+    
+    totalItems: {
+        type:Number, 
+        required:true ,
+        comment: "Holds total number of items in the cart"
+      },
+    
+    
+},
+    { timestamps: true })
+
+module.exports = mongoose.model('Cart', cartSchema)
--- src/models/productModel.js
+++ src/models/productModel.js
@@ -2,18 +2,42 @@ const mongoose = require('mongoose');
 
 
 const productSchema = new mongoose.Schema({
-    title: {type:String, require:true, unique:true},
-    description: {type:String, require:true},
-    price: {type:Number, require:true},             ////, valid number/decimal
-    currencyId: {type:String, require:true },           //////  INR
-    currencyFormat: {type:String, require:true },       ///Rupee symbol
-    isFreeShipping: {type:Boolean, default: false},
-    productImage: {type:String, require:true},  // s3 link
-    style: {type:String},
-    availableSizes: {type:[String], enum:["S", "XS","M","X", "L","XXL", "XL"]}, ////// at least one size,
-    installments: {type:Number},
-    deletedAt: {type:String, default: null},              /////when the document is deleted
-    isDeleted: {type:Boolean, default: false}
-  }, { timestamps: true })
+  title: {
+    type: String, require: true, unique: true
+  },
+  description: {
+    type: String, require: true
+  },
+  price: {
+    type: Number, require: true
+  },             ////, valid number/decimal
+  currencyId: {
+    type: String, require: true
+  },           //////  INR
+  currencyFormat: {
+    type: String, require: true
+  },       ///Rupee symbol
+  isFreeShipping: {
+    type: Boolean, default: false
+  },
+  productImage: {
+    type: String, require: true
+  },  // s3 link
+  style: {
+    type: String
+  },
+  availableSizes: {
+    type: [String], enum: ["S", "XS", "M", "X", "L", "XXL", "XL"]
+  }, ////// at least one size,
+  installments: {
+    type: Number
+  },
+  deletedAt: {
+    type: String, default: null
+  },              /////when the document is deleted
+  isDeleted: {
+    type: Boolean, default: false
+  }
+}, { timestamps: true })
 
-  module.exports = mongoose.model("Products", productSchema)
+module.exports = mongoose.model("Products", productSchema)
